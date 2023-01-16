0662c4bb1bc321990914e4ab1586b1beb49d11a9
Bluheir <slimcognitodudesvids@gmail.com>
2023-01-16 16:37:16 +1100

    Fixed dependencies and removed ID requirement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![feature(async_closure)]
 
-use std::{net::{Ipv4Addr, Ipv6Addr}, io::Read, sync::Arc, ops::Deref};
+use std::{net::{Ipv4Addr, Ipv6Addr}, sync::Arc, collections::HashMap};
 
-use data::{Config, DnsUpdate, Response};
-use tokio::{fs::File, io::AsyncReadExt, time::{self, Duration}, sync::mpsc::{self, Sender, Receiver}};
+use data::{Config, DnsUpdate, Response, DnsRecord, ExtendedResponse};
+use tokio::{fs::File, io::AsyncReadExt, time::{self, Duration}, sync::mpsc::{self, Sender}};
 use error::ConfigError;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer, filter};
 
@@ -47,10 +47,33 @@ async fn main() {
     let mut channels = Vec::new();
 
     for domain in config.domains.iter() {
+        let map : HashMap<String, String> = match get_dns_records(&client, &domain.zone_id, &domain.api_key).await {
+            Ok(v) => {
+                if v.response.success {
+                    HashMap::from_iter(v.result.into_iter().filter(|v| v.record_type == "A" || v.record_type == "AAAA").map(|v| (v.name, v.id)))
+                } else {
+                    tracing::warn!("Erroneous message received from Cloudflare API when querying IDs of entries for zone id {}: {}", domain.zone_id, v.response.errors[0]);
+                    continue;
+                }
+            }
+            Err(e) => {
+                tracing::error!("Unable to reach Cloudflare API while querying IDs with error {}", e);
+                break;
+            }
+        };
+
         for entry in domain.entries.iter() {
+            let id = match map.get(&entry.name) {
+                Some(v) => {
+                    v.clone()
+                }
+                None => {
+                    tracing::warn!("Unable to find ID of entry {} for zone id {}. Please make sure the entry name in the config matches the entry in Cloudflare EXACTLY.", entry.name, domain.zone_id);
+                    continue;
+                }
+            };
             let (send, mut recv) = mpsc::channel::<Arc<(Option<Ipv4Addr>, Option<Ipv6Addr>)>>(1);
 
-            let config = config.clone();
             let domain = domain.clone();
             let entry = entry.clone();
             let client = client.clone();
@@ -58,10 +81,13 @@ async fn main() {
             channels.push(send);
 
             tokio::task::spawn(async move {
-                let ipv6 = if entry.record_type== "AAAA" {
+                let ipv6 = if entry.record_type == "AAAA" {
                     true
-                } else {
+                } else if entry.record_type == "A" {
                     false
+                } else {
+                    tracing::warn!("Entry {} of zone {} has an improper record type ({:?}). Ignoring entry.", entry.name, domain.zone_id, entry.record_type);
+                    return;
                 };
 
                 let mut to_change : bool = false;
@@ -69,7 +95,7 @@ async fn main() {
                 
                 while let Some(v) = recv.recv().await {
                     if to_change {
-                        match do_update(&client, &domain.zone_id, &domain.id, &domain.api_key, update.clone()).await {
+                        match do_update(&client, &domain.zone_id, &id, &domain.api_key, update.clone()).await {
                             Ok(v) => {
                                 if v.success {
                                     tracing::info!("Successfully changed the IP address of entry {} to {}.", update.entry.name, update.content);
@@ -110,7 +136,7 @@ async fn main() {
                     if addr != update.content {
                         update.content = addr;
 
-                        match do_update(&client, &domain.zone_id, &domain.id, &domain.api_key, update.clone()).await {
+                        match do_update(&client, &domain.zone_id, &id, &domain.api_key, update.clone()).await {
                             Ok(v) => {
                                 if v.success {
                                     tracing::info!("Successfully changed the IP address of entry {} to {}.", update.entry.name, update.content);
@@ -159,6 +185,15 @@ async fn read_to_config(path : &str) -> Result<Config, ConfigError> {
 async fn do_update(client : &reqwest::Client, zone_id : &str, id : &str, auth : &str, update : DnsUpdate) -> Result<Response, reqwest::Error> {
     let v = client.put(format!("{}/zones/{}/dns_records/{}", ROOT, zone_id, id))
         .json(&update)
+        .header("Authorization", format!("Bearer {}", auth))
+        .send()
+        .await?;
+
+    Ok(v.json().await.unwrap())
+}
+
+async fn get_dns_records(client : &reqwest::Client, zone_id : &str, auth : &str) -> Result<ExtendedResponse<Vec<DnsRecord>>, reqwest::Error> {
+    let v = client.get(format!("{}/zones/{}/dns_records", ROOT, zone_id))
         .header("Authorization", format!("Bearer {}", auth))
         .send()
         .await?;
